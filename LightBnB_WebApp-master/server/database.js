88fa3e281cc34c43dcbccd42b9b95c82343a6571
deonleer <deon.leer96@gmail.com>
2020-11-28 20:29:14 +0000

    changed to sqp wueries

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -7,7 +7,7 @@ const pool = new Pool({
   user: 'vagrant',
   password: '123',
   host: 'localhost',
-  database: 'lightbnb'
+  database: 'lightbnb',
 });
 
 /// Users
@@ -18,16 +18,13 @@ const pool = new Pool({
  * @return {Promise<{}>} A promise to the user.
  */
 const getUserWithEmail = function(email) {
-  let user;
-  for (const userId in users) {
-    user = users[userId];
-    if (user.email.toLowerCase() === email.toLowerCase()) {
-      break;
-    } else {
-      user = null;
-    }
-  }
-  return Promise.resolve(user);
+  return pool.query(`
+  SELECT *
+  FROM users 
+  WHERE email = $1`, [email])
+  .then((data) => {
+    return data.rows[0];
+  });
 }
 exports.getUserWithEmail = getUserWithEmail;
 
@@ -37,7 +34,13 @@ exports.getUserWithEmail = getUserWithEmail;
  * @return {Promise<{}>} A promise to the user.
  */
 const getUserWithId = function(id) {
-  return Promise.resolve(users[id]);
+  return pool.query(`
+    SELECT * FROM users
+    WHERE id = $1
+  `, [id])
+    .then((data) => {
+      return data.rows[0];
+    });
 }
 exports.getUserWithId = getUserWithId;
 
@@ -48,10 +51,14 @@ exports.getUserWithId = getUserWithId;
  * @return {Promise<{}>} A promise to the user.
  */
 const addUser =  function(user) {
-  const userId = Object.keys(users).length + 1;
-  user.id = userId;
-  users[userId] = user;
-  return Promise.resolve(user);
+  return pool.query(`
+    INSERT INTO users (name, email, password)
+    VALUES ($1, $2, $3)
+    RETURNING *
+  `, [user.name, user.email, user.password])
+    .then((data) => {
+      return data.rows[0]
+    })
 }
 exports.addUser = addUser;
 
@@ -78,8 +85,8 @@ exports.getAllReservations = getAllReservations;
 const getAllProperties = function(options, limit = 10) {
   return pool.query(`
   SELECT * FROM properties
-  LIMIT $1
-  `, [limit])
+  LIMIT $1`
+  , [limit])
   .then(res => res.rows);
 }
 exports.getAllProperties = getAllProperties;
